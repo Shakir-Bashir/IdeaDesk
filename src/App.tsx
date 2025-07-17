3624c844e433d4b000c8860902574b61565aa92a
Shakir-Bashir <shakir.786.external@gmail.com>
2025-07-17 19:40:08 +0530

    complete navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { Page } from "./Page/Page";
 import { AppStateProvidedr } from "./state/AppStateContext";
 import { createPage } from "./utils/createPage";
+import { Route, Routes } from "react-router-dom";
 
 const initialState = createPage();
 
+const Auth = () => <div>Auth</div>;
+
 function App() {
   return (
-    <AppStateProvidedr initialState={initialState}>
-      <Page />;
-    </AppStateProvidedr>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+
+      <Route
+        path="/:id"
+        element={
+          <AppStateProvidedr initialState={initialState}>
+            <Page />;
+          </AppStateProvidedr>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <AppStateProvidedr initialState={initialState}>
+            <Page />;
+          </AppStateProvidedr>
+        }
+      />
+    </Routes>
   );
 }
 
